c56811fcc1cd7d7aa5453565734fc5e1490b9f16
tiborbozik <160650542+tiborbozik@users.noreply.github.com>
2024-09-18 15:04:29 -0400

    employee

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+// Create an interface called IQuittable
+interface IQuittable
+{
+    void Quit();
+}
+
+// Modify the Employee class to inherit from the IQuittable interface
+class Employee : IQuittable
+{
+    public int Id { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+
+    // Implement the Quit() method defined in the IQuittable interface
+    public void Quit()
+    {
+        Console.WriteLine("Employee quitting...");
+        // Add any additional logic or actions you want to perform when an employee quits
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        // Create an object of type IQuittable using polymorphism
+        IQuittable quittable = new Employee();
+
+        // Call the Quit() method on the IQuittable object
+        quittable.Quit();
+    }
+}
